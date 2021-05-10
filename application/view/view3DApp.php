e36f1536615a864f1861139454b0c8dbee34c1a9
Koreex Jin <koreex.jin@gmail.com>
2021-05-10 16:33:46 +0800

    remove old codes

--- application/view/view3DApp.php
+++ application/view/view3DApp.php
@@ -1,26 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<!--
-<head>
-	<meta charset="UTF-8">
-	<title>Test View</title>
-</head>
-<body>
-	<h1>Initial 3D App Test View</h1>
-	<?php
-		echo $model1 ."<br>";
-		echo $image3D1 ."<br>";
-		echo $model2 ."<br>";
-		echo $model3 ."<br>";
-		echo $model4 ."<br>";
-		//echo $model5 ."<br>";
-		//echo $model6 ."<br>";
-	?>
-	<p>If you are seeing the test data above Model 3d Image Data, then your bacic MVC is working</p>
-</body>
-</html>
--->
-
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
