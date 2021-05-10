a5c79c06f9684a593c39b472ba8640deff9f026b
Koreex Jin <koreex.jin@gmail.com>
2021-05-10 16:32:49 +0800

    add php codes

---
+++ application/model/model.php
@@ -0,0 +1,25 @@
+<?php
+
+class Model{
+
+	public function model3dInfo()
+	{
+
+		return array(
+			'name1' => 'coke',
+			'description1' => 'Coca Cola X3D Model',
+			'path1' => 'coke_top_tn',
+
+
+			'name2' => 'Coke Can 3D Image 2',
+			'description2' => 'Sprite X3D Model',
+			'path2' => 'coke_top_tn2',
+
+			'name3' => 'Sprite Bottle 3D Image 1',
+			'description3' => 'Dr Pepper X3D Model',
+			'path3' => 'coke_top_tn3',
+		);
+	}
+}
+
+?>
