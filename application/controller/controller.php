bdc1810654860c14cc1c6ad0575ad46814a80b5d
Koreex Jin <koreex.jin@gmail.com>
2021-05-13 16:02:48 +0800

    add database

--- application/controller/controller.php
+++ application/controller/controller.php
@@ -2,24 +2,62 @@
 
 class Controller {
 
+
 	public $load;
 	public $model;
 
-	function __construct()
+
+	function __construct($pageURI = null)
 	{
 
 		$this->load = new Load();
 		$this->model = new Model();
-		$this->home();
+		$this->$pageURI();
 	}
 
 	function home()
 	{
 
-		$data = $this->model->model3dInfo();
+		$data = $this->model->model3D_info();
 
-		$this->load->view('view3DApp', $data);
+		$this->load->view('view3DApp',$data);
 	}
-}
 
+	function apiCreateTable()
+	{
+		$data = $this->model->dbCreateTable();
+
+		$this->load->view('viewMessage',$data);
+	}
+
+	function apiInsertData()
+	{
+		$data = $this->model->dbInsertData();
+
+		$this->load->view('viewMessage',$data);
+	}
+
+	function apiGetData()
+	{
+		$data = $this->model->dbGetData();
+
+		$this->load->view('view3DAppdata',$data);
+	}
+
+	function dbCreateTable()
+	{
+		echo "Create";
+	}
+
+	function dbInsertData()
+	{
+		echo "Insert";
+	}
+
+	function dbGetData()
+	{
+		echo "Get data";
+	}
+
+}
 ?>
--- application/model/model.php
+++ application/model/model.php
@@ -1,25 +1,106 @@
 <?php
-
 class Model{
 
-	public function model3dInfo()
+	public $dbhandle;
+
+	public function __construct()
 	{
+		$dsn = 'sqlite:./db/coke.db';
 
+		try{
+			$this->dbhandle = new PDO($dsn, 'user', 'password', array(
+														PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+														PDO::ATTR_EMULATE_PREPARES => false,
+														));
+		}
+		catch(PDOEXception $e) {
+			echo "I'm sorry, Martin. db not Work!";
+			print new Exception($e->getMessage());
+		}
+	}
+
+	public function dbCreateTable()
+	{
+		try{
+			$this->dbhandle->exec("CREATE TABLE Model_3D (Id INTEGER PRIMARY KEY, x3dModelTitle TEXT, x3dCreationMethod TEXT, modelTitle TEXT, modelSubtitle TEXT, modelDescription TEXT)");
+			return "Model_3D table is successfully created inside test1.db file";
+		}
+		catch (PD0EXception $e){
+			print new Expection($e->getMessage());
+		}
+		$this->dbhandle = NULL;
+	}
+
+	function dbInsertData()
+	{
+		try{
+			$this->dbhandle->exec(
+			"INSERT INTO Model_3D (Id, x3dModelTitle, x3dCreationMethod, modelTitle, modelSubtitle, modelDescription)
+				VALUES (1, 'string_1', 'string_2', 'string_3', 'string_4', 'string_5');".
+			"INSERT INTO Model_3D (Id, x3dModelTitle, x3dCreationMethod, modelTitle, modelSubtitle, modelDescription)
+				VALUES (2, 'string_1', 'string_2', 'string_3', 'string_4', 'string_5');".
+			"INSERT INTO Model_3D (Id, x3dModelTitle, x3dCreationMethod, modelTitle, modelSubtitle, modelDescription)
+				VALUES (3, 'string_1', 'string_2', 'string_3', 'string_4', 'string_5'); ");
+			return "X3D model dara inserted successfully inside test1.db";
+		}
+		catch (PD0EXception $e){
+			print new Expection($e->getMessage());
+		}
+		$this->dbhandle = NULL;
+	}
+
+	function dbGetData()
+	{
+		try{
+
+			$sql = 'SELECT * FROM Model_3D';
+
+			$stmt = $this->dbhandle->query($sql);
+
+			$result = null;
+
+			$i=0;
+
+
+			while ($data = $stmt->fetch()) {
+				$result[$i]['x3dModelTitle'] = $data['x3dModelTitle'];
+				$result[$i]['x3dCreationMethod'] = $data['x3dCreationMethod'];
+				$result[$i]['modelTitle'] = $data['modelTitle'];
+				$result[$i]['modelSubtitle'] = $data['modelSubtitle'];
+				$result[$i]['modelDescription'] = $data['modelDescription'];
+
+				$i++;
+			}
+			return $result;
+		}
+		catch (PD0EXception $e){
+			print new Expection($e->getMessage());
+		}
+		$this->dbhandle = NULL;
+	}
+
+	public function model3D_info()
+	{
 		return array(
-			'name1' => 'coke',
-			'description1' => 'Coca Cola X3D Model',
-			'path1' => 'coke_top_tn',
+			'model1' => 'Coke Can 3D Image 1',
+			'image3D1' => 'coke_top_tn',
 
 
-			'name2' => 'Coke Can 3D Image 2',
-			'description2' => 'Sprite X3D Model',
-			'path2' => 'coke_top_tn2',
+			'model2' => 'Coke Can 3D Image 2',
+			'image3D2' => 'coke_top_tn2',
 
-			'name3' => 'Sprite Bottle 3D Image 1',
-			'description3' => 'Dr Pepper X3D Model',
-			'path3' => 'coke_top_tn3',
+			'model3' => 'Sprite Bottle 3D Image 1',
+			'image3D3' => 'coke_top_tn3',
+
+			'model4' => 'Sprite Bottle 3D Image 2',
+			'image3D4' => 'coke_top_tn4',
+
+
+			//'model5' => 'Dr Pepper Cup 3D Image 1',
+			//'model6' => 'Dr Pepper Cup 3D Image 2',
 		);
+
+
 	}
 }
-
 ?>
